83da5f803968d9206e8bc1bad23b245e4757b583
Darren Jude <darjk94@gmail.com>
2025-05-27 21:24:48 +1000

    Fixed route and added missing index.html

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// In-memory storage
-let requests = [];
-let users = [];
-
 // Default route to serve index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
